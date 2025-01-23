639f21533d3c6b8c8557ead41afa91a000612baf
Martijn Spitter <info@martijnspitter.nl>
2025-01-23 22:55:03 +0100

    [1267] initial solution

--- 1267/main.go
+++ 1267/main.go
@@ -7,5 +7,31 @@ func main() {
 }
 
 func countServers(grid [][]int) int {
-	return 0
+	count := 0
+	rows := make(map[int]int)
+	cols := make(map[int]int)
+
+	for i, row := range grid {
+		for j, val := range row {
+			if val == 1 {
+				if rows[i] == 1 {
+					count = count + 2
+				}
+				if rows[i] > 1 {
+					count++
+				}
+				rows[i] = rows[i] + 1
+
+				if cols[j] == 1 {
+					count = count + 2
+				}
+				if cols[j] > 1 {
+					count++
+				}
+				cols[j] = rows[j] + 1
+			}
+		}
+	}
+
+	return count
 }
