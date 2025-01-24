62b7abfd0762ff7df12376e2236b6db27fda78ca
Martijn Spitter <info@martijnspitter.nl>
2025-01-24 13:54:16 +0100

    [802] init problem

---
+++ 802/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+type Node struct {
+}
+
+func eventualSafeNodes(graph [][]int) []int {
+	return nil
+}
---
+++ 802/main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func Test802(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    [][]int
+		expected []int
+	}{
+		{
+			name: "Test 1",
+			input: [][]int{
+				{1, 2},
+				{2, 3},
+				{5},
+				{0},
+				{5},
+				{},
+				{},
+			},
+			expected: []int{2, 4, 5, 6},
+		},
+	}
+
+	for _, test := range tests {
+		result := eventualSafeNodes(test.input)
+
+		if len(result) != len(test.expected) {
+			t.Errorf("Expected slice of size %d but got %d", len(test.expected), len(result))
+		}
+
+		for i, val := range result {
+			if test.expected[i] != val {
+				t.Errorf("Expect %d but got %d", test.expected[i], val)
+			}
+		}
+	}
+}
