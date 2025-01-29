ea040b02e0fbe862ecae50d5c32ce8d2736f055e
Martijn Spitter <info@martijnspitter.nl>
2025-01-29 21:30:28 +0100

    [1] final solution

--- 1/main.go
+++ 1/main.go
@@ -7,50 +7,19 @@ func main() {
 }
 
 func twoSum(nums []int, target int) []int {
-	indices := []int{}
-	sum := 0
+	indices := map[int]int{}
 
-	for i := range nums {
-		if nums[i] > target {
+	for i, val := range nums {
+		remainder := target - val
+
+		j, ok := indices[remainder]
+		if !ok {
+			indices[val] = i
 			continue
 		}
-		if nums[i] == target {
-			return []int{i}
-		}
-		for j := range nums {
-			if i == j {
-				continue
-			}
-			if nums[j] > target {
-				continue
-			}
-			if nums[j] == target {
-				return []int{j}
-			}
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
-			if nums[i]+nums[j] > target {
-				continue
-			}
-			if nums[i]+nums[j] < target {
-				sum = nums[i] + nums[j]
-				indices = append(indices, i, j)
-				break
-			}
-			if nums[i]+nums[j]+sum < target {
-				sum = sum + nums[i] + nums[j]
-				indices = append(indices, i, j)
-				break
-			}
-			if nums[i]+nums[j]+sum == target {
-				sum = sum + nums[i] + nums[j]
-				indices = append(indices, i, j)
-				return indices
-			}
 
-		}
+		return []int{i, j}
 	}
 
-	return indices
+	return nil
 }
