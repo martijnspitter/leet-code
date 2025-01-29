f32746f26a0c31eb173b2fadfdb83ceeadb6ae74
Martijn Spitter <info@martijnspitter.nl>
2025-01-29 20:44:34 +0100

    [1] init problem

---
+++ 1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+func twoSum(nums []int, target int) []int {
+	return nums
+}
---
+++ 1/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import "testing"
+
+func Test1(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  []int
+		target int
+		output []int
+	}{
+		{
+			name:   "Test 1",
+			input:  []int{2, 7, 11, 15},
+			target: 9,
+			output: []int{0, 1},
+		},
+		{
+			name:   "Test 2",
+			input:  []int{3, 2, 4},
+			target: 6,
+			output: []int{1, 2},
+		},
+		{
+			name:   "Test 3",
+			input:  []int{3, 3},
+			target: 6,
+			output: []int{0, 1},
+		},
+	}
+
+	for _, test := range tests {
+		output := twoSum(test.input, test.target)
+		if len(output) != len(test.output) {
+			t.Errorf("%s failed. Expected array of length %d but got %d", test.name, len(test.output), len(output))
+			t.FailNow()
+		}
+
+		for i, val := range output {
+			if val != test.output[i] {
+				t.Errorf("%s failed. Expected %d but got %d", test.name, test.output[i], val)
+			}
+		}
+	}
+}
