6c04e8be226ba86385101aacfdaf3b414e72f960
Martijn Spitter <info@martijnspitter.nl>
2025-02-01 15:39:55 +0100

    [3] initial solution

--- 3/main.go
+++ 3/main.go
@@ -1,11 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello World")
 }
 
 func lengthOfLongestSubstring(s string) int {
-	return 0
+	max := 0
+	current := ""
+	for _, char := range strings.Split(s, "") {
+		if strings.Contains(current, char) {
+			if len(current) > max {
+				max = len(current)
+			}
+			current = char
+		} else {
+			current = current + char
+		}
+	}
+	return max
 }
