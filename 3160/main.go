92dd9978a6653d464768e638952bbcda2e76412e
Martijn Spitter <info@martijnspitter.nl>
2025-02-07 21:20:59 +0100

    [3160] final solution

--- 3160/main.go
+++ 3160/main.go
@@ -7,20 +7,38 @@ func main() {
 }
 
 func queryResults(limit int, queries [][]int) []int {
-	result := []int{}
-
+	result := make([]int, 0, len(queries))
 	balls := make(map[int]int)
+	colorCount := make(map[int]int)
+	distinctColors := 0
 
 	for _, query := range queries {
-		balls[query[0]] = query[1]
+		ball, newColor := query[0], query[1]
 
-		colors := make(map[int]int)
-
-		for key := range balls {
-			colors[balls[key]] = 1
+		// Check if the ball already has a color
+		if oldColor, exists := balls[ball]; exists {
+			if oldColor != newColor {
+				// Decrease the count of the old color
+				colorCount[oldColor]--
+				// If the old color count drops to zero, it's no longer a distinct color
+				if colorCount[oldColor] == 0 {
+					distinctColors--
+				}
+			}
 		}
 
-		result = append(result, len(colors))
+		// Update the ball's color only if it changes
+		if oldColor, exists := balls[ball]; !exists || oldColor != newColor {
+			balls[ball] = newColor
+			// Increase the count of the new color
+			if colorCount[newColor] == 0 {
+				distinctColors++
+			}
+			colorCount[newColor]++
+		}
+
+		// Append the number of distinct colors to the result
+		result = append(result, distinctColors)
 	}
 
 	return result
